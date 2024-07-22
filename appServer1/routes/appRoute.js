9a8843a7bf67f88f684d4e0879a4cba26cd0ea4a
farhanjums <83050550+farhanjums@users.noreply.github.com>
2024-07-22 19:24:17 +0800

    Add files via upload

---
+++ appServer1/routes/appRoute.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const router = express.Router();
+var path = require('path')
+const bodyParser = require("body-parser");
+let multer = require('multer');
+let upload = multer();
+const mysql = require('mysql2');
+const db = require('../views/DbConfig.js');
+const secret = require('../views/secret.js');
+
+router.use(express.static("views"));
+
+secret("farhan-Secret-DB").then(
+  values => values).then((dbValues) =>
+
+    con = mysql.createConnection({
+    host: dbValues.host,
+    user: dbValues.username,
+    password: dbValues.password,
+    database: 'project3'
+  })
+
+);
+
+/*
+var con = mysql.createConnection({
+  host: values.host,
+  user: values.username,
+  password: values.password,
+  database: 'project3'
+});
+*/
+
+router.use(express.json());
+router.use(bodyParser.urlencoded({extended:true}));
+
+router.get('/health',(req,res) => {
+
+  res.status(200).send(" Health check success");
+});
+
+
+
+router.post('/submit',upload.fields([]),(req, res) => {
+    if (Object.keys(req.body).length === 0) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        return res.status(400).send('Request body is empty');
+      } 
+      
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.location('http://localhost:5001/api/feedback')
+      console.log(req.body);
+      var query1="INSERT INTO `ict2509c` (`ques1`, `ques2`, `ques3`, `name`) VALUES ('"+req.body.Q1+"','"+req.body.Q2+"','"+ req.body.Q3+"','"+ req.body.text+"')";
+      console.log(query1);
+      con.query(query1,(err,result)=>{
+        console.log(result);
+      }
+      );
+      res.status(200).send({apiMessage: 'done'});
+
+});
+
+
+router.post('/register',upload.fields([]),(req, res) => {
+  if (Object.keys(req.body).length === 0) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      return res.status(400).send('Request body is empty');
+    } 
+    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.location('http://localhost:8080/api/Log-In')
+    console.log(req.body);
+    var query1="INSERT INTO `login_2511c` (`username`, `password`, `email`) VALUES ('"+req.body.username+"','"+req.body.password+"','"+ req.body.email+"')";
+    console.log(query1);
+    con.query(query1,(err,result)=>{
+      if(result){
+        console.log(result);
+        res.status(200).send({apiMessage: 'done'});
+      }
+      else{
+        console.log(err);
+        res.status(200).send({apiMessage: 'fail',code: err.code,sqlMessage: err.sqlMessage});
+      }
+    }
+    );
+
+});
+
+
+router.post('/login',upload.fields([]),(req, res) => {
+  if (Object.keys(req.body).length === 0) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      return res.status(400).send('Request body is empty');
+    } 
+    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.location('http://localhost:8080/api/Log-In')
+    console.log(req.body);
+    var query1="SELECT * FROM `login_2511c` WHERE `username`='"+req.body.username+"'";
+    console.log(query1);
+    con.query(query1,(err,result)=>{
+      if(result.length>=1){
+        console.log(result);
+        console.log(result[0].password);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send({apiMessage: 'done',password:result[0].password});
+      }
+      else{
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send({apiMessage: 'fail',sqlMessage: 'User not found'});
+      }
+    }
+    );
+
+});
+
+router.post('/deserialize',upload.fields([]),(req, res) => {
+  if (Object.keys(req.body).length === 0) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      return res.status(400).send('Request body is empty');
+    } 
+    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.location('http://localhost:8080/api/Log-In')
+    console.log(req.body.user);
+    var query1="SELECT * FROM `login_2511c` WHERE `username`='"+req.body.user.username+"'";
+    console.log(query1);
+    con.query(query1,(err,result)=>{
+      if(result.length>=1){
+        console.log(result);
+        console.log(result[0].password);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send({apiMessage: 'done',username: result[0].username, password:result[0].password});
+      }
+      else{
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send({apiMessage: 'fail',sqlMessage: 'User not found'});
+      }
+    }
+    );
+
+});
+
+
+module.exports = router;
---
+++ appServer1/views/secret.js
@@ -0,0 +1,48 @@
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+
+const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+  
+async function getSecret(secretName) {  
+const secret_name = secretName;
+  
+  
+  const client = new SecretsManagerClient({
+    region: "us-east-1"
+  });
+  
+  let response;
+  
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+
+  const secret = JSON.parse(response.SecretString);
+  return secret;
+}
+/*
+  var values =  getSecret();
+  console.log(values);
+  console.log(values.username);
+  console.log(values.password);
+  console.log(values.host);
+  console.log(values.username);
+*/
+
+
+
+
+
+
+module.exports= getSecret;
