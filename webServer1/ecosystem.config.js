4e958be7396edf61e13a214b7b74b03c727e0544
farhanjums <83050550+farhanjums@users.noreply.github.com>
2024-07-25 19:42:31 +0800

    Update ecosystem.config.js

--- webServer1/ecosystem.config.js
+++ webServer1/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
-    name: "Your-App-Name",
-    script: "Start-Up-File-Name",
+    name: "index",
+    script: "index.js",
     error_file: "/var/log/pm2/error.log",
     out_file: "/var/log/pm2/pm2.log",
     watch: true,
